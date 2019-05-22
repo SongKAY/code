1f4c9a0256455ee59439413de295dd319fd137e2
SongKAY <50950911@qq.com>
2019-05-22 16:10:34 +0800

    Create 413. 等差数列划分.cpp

---
+++ Leetcode/413. 等差数列划分.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        int n = A.size();
+        int result = 0;
+        vector<int> dp(n,0);
+        for(int i=2;i<n;i++){
+            if(A[i]-A[i-1]==A[i-1]-A[i-2]){
+                dp[i] = dp[i-1] + 1;
+            }
+            result += dp[i];
+        }
+        return result;
+    }
+};
