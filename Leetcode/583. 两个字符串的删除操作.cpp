58f7fa2652b402dd4517937dce74ce1c84dcfb53
SongKAY <50950911@qq.com>
2019-06-10 17:37:40 +0800

    Create 583. 两个字符串的删除操作.cpp

---
+++ Leetcode/583. 两个字符串的删除操作.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j] = max(dp[i-1][j-1]+1,dp[i][j]);
+                else
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        int result = n - dp[n][m] + m - dp[n][m];
+        return result;
+    }
+};
