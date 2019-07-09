6399d95fe2d7f24cc87cd9f21d0c731d4c44412a
SongKAY <50950911@qq.com>
2019-07-09 11:32:38 +0800

    Update 912. 排序数组.cpp

--- Leetcode/912. 排序数组.cpp
+++ Leetcode/912. 排序数组.cpp
@@ -30,3 +30,36 @@ public:
 };
 
 //归并
+class Solution {
+public:
+    void mergeSort(vector<int>&nums,int left,int mid,int right){
+        vector<int> temp;
+        int i = left;
+        int j = mid+1;
+        while(i<=mid&&j<=right){
+            if(nums[i]<nums[j])
+                temp.push_back(nums[i++]);
+            else
+                temp.push_back(nums[j++]);
+        }
+        while(i<=mid)
+            temp.push_back(nums[i++]);
+        while(j<=right)
+            temp.push_back(nums[j++]);
+        for(int i=0;i<temp.size();i++)
+            nums[i+left] = temp[i];
+    }
+    void merge(vector<int>&nums,int left,int right){
+        if(left<right){
+            int mid = (left+right)/2;
+            merge(nums,left,mid);
+            merge(nums,mid+1,right);
+            mergeSort(nums,left,mid,right);
+        }
+        return;
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        merge(nums,0,nums.size()-1);
+        return nums;
+    }
+};
