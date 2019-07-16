a148d3a7b9931a79d112e515e9f7d5877949b18d
SongKAY <50950911@qq.com>
2019-07-16 11:10:08 +0800

    Update $$$75.颜色分类.cpp

--- Leetcode/$$$75.颜色分类.cpp
+++ Leetcode/$$$75.颜色分类.cpp
@@ -23,3 +23,35 @@ public:
         return ;
     }
 };
+
+
+/*还是快排吧*/
+class Solution {
+public:
+    void quick_sort(int l,int r,vector<int>&nums){
+        if(l>r)
+            return;
+        int i = l;
+        int j = r;
+        int temp = nums[l];
+        while(i<j){
+            while(i<j&&nums[j]>=temp)
+                j--;
+            while(i<j&&nums[i]<=temp)
+                i++;
+            if(i<j){
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+            }
+        }
+        nums[l] = nums[i];
+        nums[i] = temp;
+        quick_sort(l,i-1,nums);
+        quick_sort(i+1,r,nums);
+    }
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        quick_sort(0,n-1,nums);
+    }
+};
