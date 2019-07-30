527262d30defd7a658e28d06f2fab9e5c9b97ec2
SongKAY <50950911@qq.com>
2019-07-30 10:05:46 +0800

    Create 140.单词拆分II.cpp

---
+++ Leetcode/140.单词拆分II.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<string,vector<string>> m;
+        return helper(s, wordDict, m);
+    }
+    vector<string> helper(string s,vector<string>& wordDict, unordered_map<string,vector<string>>& m){
+        if(m.count(s)) 
+            return m[s];
+        if(s=="")
+            return {""};
+        vector<string> res;
+        for(auto word:wordDict){
+            if(s.substr(0,word.size())!=word)
+                continue;
+            vector<string> temp = helper(s.substr(word.size()),wordDict,m);
+            for(auto str:temp){
+                res.push_back(word+ (str.empty()?"":" ") + str);
+            }
+        }
+        return m[s] = res;
+    }
+};
