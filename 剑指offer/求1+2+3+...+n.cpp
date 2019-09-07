60fbfb22a5554814e08d37a990793e402952a632
SongKAY <50950911@qq.com>
2019-09-07 09:56:32 +0800

    Update 求1+2+3+...+n.cpp

--- 剑指offer/求1+2+3+...+n.cpp
+++ 剑指offer/求1+2+3+...+n.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    int Sum(int &sum,int n){
-        n&&Sum(sum,n-1);
-        return sum += n;
-    }
     int Sum_Solution(int n) {
-        int sum = 0;
-        return Sum(sum,n);
+        if(n==0)
+            return 0;
+        return Sum_Solution(n-1) + n;
     }
 };
