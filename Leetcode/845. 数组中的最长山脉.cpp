ff567d16c2b15d419ba22824f68917a6257f4342
SongKAY <50950911@qq.com>
2019-09-17 11:31:38 +0800

    Update 845. 数组中的最长山脉.cpp

--- Leetcode/845. 数组中的最长山脉.cpp
+++ Leetcode/845. 数组中的最长山脉.cpp
@@ -1,50 +1,24 @@
 class Solution {
 public:
     int longestMountain(vector<int>& A) {
-        if(A.size()<3)
-            return 0;
-        vector<int> v;
-        for(int i=0;i<A.size()-1;i++){
-            if(A[i]<A[i+1])
-                v.push_back(1);
-            else if(A[i]==A[i+1])
-                v.push_back(0);
-            else
-                v.push_back(-1);
-        }
-        int result = 0;
-        int l = -1;
-        for(int i=0;i<v.size();){
-            if(v[i]==0||v[i]==-1){
+        int n = A.size();
+        int maxlen = 0;
+        for(int i=1;i<n;){
+            int j = i;
+            if(A[j]<=A[j-1])
                 i++;
-                continue;
-            }
             else{
-                int cnt = 0;
-                int pre = 1;
-                while(i<v.size()){
-                    if(v[i]==0){
-                        break;
-                    }
-                    else if(v[i]==-1){
-                        if(pre==1)
-                            pre = -1;
-                        cnt ++;
-                    }
-                    else{
-                        if(pre==1)
-                            cnt++;
-                        else
-                            break;
-                    }
-                    i++;
-                }
-                if(pre==-1)
-                    result = max(result,cnt);
+                int l = j-1;
+                while(j<n&&A[j]>A[j-1])
+                    j++;
+                int mid = j-1;
+                while(j<n&&A[j]<A[j-1])
+                    j++;
+                if(mid+1<j)
+                    maxlen = max(maxlen,j-l);
+                i = j;
             }
         }
-        if(!result)
-            return result;
-        return result+1;
+        return maxlen;
     }
 };
