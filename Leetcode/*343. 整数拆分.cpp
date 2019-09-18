64a5fd84883270844e90a0954fdd9787240b2305
SongKAY <50950911@qq.com>
2019-09-18 09:47:26 +0800

    Update *343. 整数拆分.cpp

--- Leetcode/*343. 整数拆分.cpp
+++ Leetcode/*343. 整数拆分.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        vector<int> dp(n+1,1);
-        for(int i=2;i<=n;i++){
+        if(n<=3)
+            return n-1;
+        vector<int> dp(n+1,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i=4;i<=n;i++){
             for(int j=1;j<i;j++){
-                int t = max(j*(i-j),j*dp[i-j]);
-                dp[i] = max(t,dp[i]);
+                dp[i] = max(dp[i],max((i-j)*dp[j],(i-j)*j));
             }
         }
         return dp[n];
