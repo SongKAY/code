542dd84c3c3d5d836161c072b8727478e59894e7
SongKAY <50950911@qq.com>
2019-09-19 12:00:03 +0800

    Update and rename 873. 最长的斐波那契子序列的长度.cpp to **873. 最长的斐波那契子序列的长度.cpp

--- Leetcode/**873. 最长的斐波那契子序列的长度.cpp
+++ Leetcode/**873. 最长的斐波那契子序列的长度.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& A) {
         int n = A.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        int maxlen = 0;
+        vector<vector<int>> dp(n,vector<int>(n,0));
         map<int,int> m;
-        int result = 0;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             m[A[i]] = i;
+        }
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 dp[j][i] = 2;
                 if(A[i]-A[j]<A[i]&&m.find(A[i]-A[j])!=m.end()){
                     int k = m[A[i]-A[j]];
-                    dp[j][i] = max(dp[j][i],dp[k][j]+1);
-                    result = max(result,dp[j][i]);
+                    dp[j][i] = max(dp[k][j]+1,dp[j][i]);
                 }
+                maxlen = max(dp[j][i],maxlen);
             }
         }
-        return (result>2)?result:0;
+        return maxlen>2?maxlen:0;
     }
 };
