30b2ad29548ce2457a70ca84683cf79cb4f59676
SongKAY <50950911@qq.com>
2019-09-19 16:50:05 +0800

    Update and rename 1043. 分隔数组以得到最大和.cpp to **1043. 分隔数组以得到最大和.cpp

--- Leetcode/**1043. 分隔数组以得到最大和.cpp
+++ Leetcode/**1043. 分隔数组以得到最大和.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& A, int K) {
+        if(A.empty())
+            return 0;
         int n = A.size();
-        vector<int> f(n+1,0);
+        vector<int> dp(n+1,0);
         for(int i=1;i<=n;i++){
             int curmax = -1;
             for(int j=1;j<=K&&i-j>=0;j++){
-                curmax = max(curmax,A[i-j]);
-                f[i] = max(f[i],f[i-j]+curmax*j);
+                curmax = max(A[i-j],curmax);
+                dp[i] = max(dp[i],dp[i-j]+curmax*j);
             }
         }
-        return f[n];
+        return dp[n];
     }
 };
