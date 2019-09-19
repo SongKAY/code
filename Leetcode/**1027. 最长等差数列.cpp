e823707f864e9bec4faf19357620ac3d2832e7eb
SongKAY <50950911@qq.com>
2019-09-19 11:54:58 +0800

    Update and rename 1027. 最长等差数列.cpp to **1027. 最长等差数列.cpp

--- Leetcode/**1027. 最长等差数列.cpp
+++ Leetcode/**1027. 最长等差数列.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int longestArithSeqLength(vector<int>& A) {
+        if(A.empty())
+            return 0;
         int n = A.size();
-        map<int,map<int,int>> dp;
         int result = 0;
-        for(int i=1;i<n;i++){
+        map<int,map<int,int>> dp;
+        for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                int diff = A[i] - A[j];
-                if(dp[j][diff]!=0)
+                int diff = A[i]-A[j];
+                if(dp[j][diff])
                     dp[i][diff] = max(dp[j][diff]+1,dp[i][diff]);
                 if(dp[i][diff]==0)
                     dp[i][diff] = 2;
