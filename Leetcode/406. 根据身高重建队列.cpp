a37f55cd7435eabf99f4c01ced33032bd0df00a6
SongKAY <50950911@qq.com>
2019-09-19 09:29:24 +0800

    Update 406. 根据身高重建队列.cpp

--- Leetcode/406. 根据身高重建队列.cpp
+++ Leetcode/406. 根据身高重建队列.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     static bool cmp(vector<int> a,vector<int> b){
-        if(a[0]!=b[0])
-            return a[0]>b[0];
-        else
-            return a[1]<b[1];
+        if(a[0]==b[0])
+            return a[1] < b[1];
+        return a[0]>b[0];
     }
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        int n = people.size();
+        vector<vector<int>> q;
+        if(people.empty())
+            return q;
         sort(people.begin(),people.end(),cmp);
-        vector<vector<int>> result;
-        for(int i=0;i<n;i++){
-            result.insert(result.begin()+people[i][1],people[i]);
+        for(auto c:people){
+            q.insert(q.begin()+c[1],c);
         }
-        return result;
+        return q;
     }
 };
