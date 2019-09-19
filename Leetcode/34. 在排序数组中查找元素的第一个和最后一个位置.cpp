9e81e9b51c01984b00b00800fb8b72a53b3a86d7
SongKAY <50950911@qq.com>
2019-09-19 11:05:00 +0800

    Update 34. 在排序数组中查找元素的第一个和最后一个位置.cpp

--- Leetcode/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
+++ Leetcode/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    int right_bound(vector<int>& nums,int target){
+    int findleft(vector<int>nums,int target){
         int l = 0;
         int r = nums.size()-1;
-        while(l<=r){
+        while(l<r){
             int mid = l + (r-l)/2;
-            if(nums[mid]==target){
-                if(mid==nums.size()-1||nums[mid+1]!=target)
-                    return mid;
+            if(nums[mid]<target)
                 l = mid + 1;
-            }
-            else if(nums[mid]<target)
-                l = mid + 1;
-            else
-                r = mid - 1;
+            else if(nums[mid]>=target)
+                r = mid;
         }
+        if(nums[l]==target)
+            return l;
         return -1;
     }
-    int left_bound(vector<int>& nums,int target){
+    int findright(vector<int>nums,int target){
         int l = 0;
         int r = nums.size()-1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(nums[mid]==target){
-                if(mid==0||nums[mid-1]!=target)
-                    return mid;
-                r = mid - 1;
-            }
-            else if(nums[mid]<target)
-                l = mid + 1;
-            else
-                r = mid - 1;
+        while(l<r){
+            int mid = l + (r-l+1)/2;
+            if(nums[mid]<=target)
+                l = mid;
+            else if(nums[mid]>target)
+                r = mid-1;
         }
+        if(nums[l]==target)
+            return l;
         return -1;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left = left_bound(nums,target);
-        int right = right_bound(nums,target);
-        return vector<int>{left,right};
+        if(nums.empty())
+            return vector<int>{-1,-1};
+        int l = findleft(nums,target);
+        int r = findright(nums,target);
+        return vector<int>{l,r};
+        
     }
 };
