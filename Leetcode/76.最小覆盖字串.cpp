32113136a9a03b6bf2c88e13130ddb1b11f3a385
SongKAY <50950911@qq.com>
2019-09-19 09:12:16 +0800

    Create 76.最小覆盖字串.cpp

---
+++ Leetcode/76.最小覆盖字串.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(s.length()<t.length())
+            return "";
+        map<char,int> need;
+        for(int i=0;i<t.length();i++){
+            need[t[i]] ++;
+        }
+        int l = 0;
+        int r = 0;
+        int result = s.length();
+        string temp;
+        int match = 0;
+        while(r<s.length()){
+            need[s[r]] -= 1;
+            if(need[s[r]]>=0)
+                match += 1;
+            if(match==t.length()){
+                while(l<=r&&need[s[l]]<0){
+                    need[s[l]] += 1;
+                    l ++;
+                }
+                if (result >= r-l+1){
+                    result = r-l+1;
+                    temp = s.substr(l,result);
+                }
+            }
+            r++;
+        }
+        return temp;
+    }
+};
+
