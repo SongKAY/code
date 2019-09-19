8caef92a14ff77a3bca874b4392c077bf4fa9d4f
SongKAY <50950911@qq.com>
2019-09-19 10:34:27 +0800

    Update and rename 三数之和.cpp to 15. 三数之和.cpp

--- Leetcode/15. 三数之和.cpp
+++ Leetcode/15. 三数之和.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
-        if(nums.empty())
-            return result;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
             if(nums[i]>0)
@@ -11,25 +9,24 @@ public:
             if(i>0&&nums[i]==nums[i-1])
                 continue;
             int target = 0 - nums[i];
-            int l = i + 1;
+            int l = i+1;
             int r = nums.size()-1;
             while(l<r){
-                int temp = nums[l] + nums[r];
-                if(temp<target)
-                    l ++;
-                else if(temp>target)
-                    r --;
-                else{
+                int sum = nums[l] + nums[r];
+                if(sum==target){
                     result.push_back(vector<int>{nums[i],nums[l],nums[r]});
-                    while(l+1<r&&nums[l]==nums[l+1]){
-                        l ++;
-                    }
-                    while(l<r-1&&nums[r]==nums[r-1]){
-                        r --;
-                    }
-                    l ++;
-                    r --;
+                    while(l<r&&nums[l]==nums[l+1])
+                        l++;
+                    while(l<r&&nums[r]==nums[r-1])
+                        r--;
+                    l++;
+                    r--;
                 }
+                else if(sum<target){
+                    l++;
+                }
+                else
+                    r--;
             }
         }
         return result;
