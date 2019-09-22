615311d3a6ffcc90ff8694bfb4f1410fac31eb7b
SongKAY <50950911@qq.com>
2019-09-22 09:12:08 +0800

    Create 128.最长连续序列.cpp

---
+++ Leetcode/128.最长连续序列.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        map<int,int> m;
+        int result = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(m[nums[i]])
+                continue;
+            int l = m[nums[i]-1];
+            int r = m[nums[i]+1];
+            m[nums[i]] = l + r + 1;
+            m[nums[i]-l] = l + r + 1;
+            m[nums[i]+r] = l + r + 1;
+            result = max(result,l+r+1);
+        }
+        return result;
+    }
+};
