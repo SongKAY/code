7c0e0672c5a3f478d2118c420d99ed441654804f
SongKAY <50950911@qq.com>
2019-09-23 10:32:45 +0800

    Update 377. 组合总和 Ⅳ.cpp

--- Leetcode/377. 组合总和 Ⅳ.cpp
+++ Leetcode/377. 组合总和 Ⅳ.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        long long dp[target+1]={0};
-        dp[0]=1;
-        sort(nums.begin(), nums.end());
+        vector<unsigned long long> dp(target+1,0);
+        dp[0] = 1;
         for(int i=1;i<=target;i++){
-            for(int num:nums){
-                if(i>=num){
-                    dp[i]+=dp[i-num]%INT_MAX;
-                }
+            for(auto a:nums){
+                if(i>=a)
+                    dp[i] += dp[i-a];
             }
         }
         return dp[target];
