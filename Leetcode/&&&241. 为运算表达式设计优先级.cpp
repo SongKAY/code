6d88fa3dc66bfed92590fa0d599404ea563997f3
SongKAY <50950911@qq.com>
2019-09-24 10:55:41 +0800

    Update and rename **241. 为运算表达式设计优先级.cpp to &&&241. 为运算表达式设计优先级.cpp

--- Leetcode/&&&241. 为运算表达式设计优先级.cpp
+++ Leetcode/&&&241. 为运算表达式设计优先级.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string input) {
-        vector<int> res;
-        for(int i=0;i<input.size();i++){
+        vector<int> ans;
+        for(int i=0;i<input.length();i++){
             char c = input[i];
-            if(c=='+'||c=='*'||c=='-'){
+            if(c=='+'||c=='-'||c=='*'){
                 vector<int> res1 = diffWaysToCompute(input.substr(0,i));
                 vector<int> res2 = diffWaysToCompute(input.substr(i+1));
                 for(auto r1:res1){
                     for(auto r2:res2){
                         if(c=='+')
-                            res.push_back(r1+r2);
+                            ans.push_back(r1+r2);
                         else if(c=='-')
-                            res.push_back(r1-r2);
+                            ans.push_back(r1-r2);
                         else
-                            res.push_back(r1*r2);
+                            ans.push_back(r1*r2);
                     }
                 }
             }
         }
-        if(res.empty())
-            res.push_back(stoi(input));
-        return res;
+        if(ans.empty())
+            ans.push_back(stoi(input));
+        return ans;
     }
 };
-
